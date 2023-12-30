8b1c35ac67f9568ed648d1b21284d5de221feaed
Abhishek kumar <abhi.arya97161@gmail.com>
2023-12-30 14:04:38 +0530

    What the hell are Models?

--- controllers/product.js
+++ controllers/product.js
@@ -1,6 +1,4 @@
-const path = require("path");
-
-const products = []
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
   res.render("add-product", {
@@ -13,16 +11,20 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.addNewProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
+  Product.fetchAll((products) => {
+    res.render("shop", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true,
+    });
   });
 };
